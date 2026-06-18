ee219e52aa17954a63a85a141918e4b73ca8eaad
rijindev <rijin.reji@appsteamtechnologies.com>
2026-06-18 23:49:17 +0530

    Add .gitignore, remove node_modules, use dynamic PORT for Render

--- app.js
+++ app.js
@@ -153,4 +153,5 @@ app.post("/process", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server running at http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
